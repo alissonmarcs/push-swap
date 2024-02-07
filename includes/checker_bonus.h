11d46a1fda9cc22eedcbc7e0908da42e9a15521e
Alisson Marcos <alissonmarcos249@gmail.com>
2024-02-07 15:24:08 -0300

    maint, bonus: only required functions

--- includes/checker_bonus.h
+++ includes/checker_bonus.h
@@ -14,7 +14,7 @@
 
 # define PUSH_SWAP_H
 
-# include "libft.h"
+# include <libft.h>
 
 # include <limits.h>
 
@@ -22,11 +22,6 @@ typedef struct s_node	t_node;
 struct s_node
 {
 	long			number;
-	const int		index;
-	int				position;
-	int				target_position;
-	int				cost_a;
-	int				cost_b;
 	t_node			*next;
 };
 
@@ -34,41 +29,23 @@ t_node		*create_node(long number);
 t_node		*get_last_node(t_node **head);
 void		link_node(t_node **head, t_node *node);
 void		free_list(t_node **head);
-int			get_list_size(t_node *head);
-void		check_args(int argc, char *argv[]);
-void		fill_stack_a(t_node **head, char *argv[]);
 
-/* push swap moviments */
+void		check_args(int argc, char *argv[]);
+void		check_numbers(t_node **stack_a);
+void		fill_stack_a(t_node **head, char *argv[]);
+short		is_sort(t_node *head);
+void		error(void);
+
 void		sa(t_node **stack_a);
 void		sb(t_node **stack_b);
-void		ss(t_node **stack_a, t_node **stack_b);
-
-void		pa(t_node **stack_a, t_node **stack_b);
-void		pb(t_node **stack_a, t_node **stack_b);
-
 void		rra(t_node **stack_a);
 void		rrb(t_node **stack_b);
-void		rrr(t_node **stack_a, t_node **stack_b);
-
 void		ra(t_node **stack_a);
 void		rb(t_node **stack_b);
+void		ss(t_node **stack_a, t_node **stack_b);
+void		pa(t_node **stack_a, t_node **stack_b);
+void		pb(t_node **stack_a, t_node **stack_b);
+void		rrr(t_node **stack_a, t_node **stack_b);
 void		rr(t_node **stack_a, t_node **stack_b);
-
-void		set_node_indexes(t_node *head, int stack_size);
-
-/* utils.c */
-short		is_sort(t_node *head);
-void		sort_three_elements(t_node **head);
-void		set_node_positions(t_node *stack);
-
-void		move_all_but_three(t_node **head_stack_a, t_node **head_stack_b);
-
-void		set_target_position(t_node *stack_a, t_node *stack_b);
-void		set_costs(t_node *stack_a, t_node *stack_b);
-void		sort(t_node **head_stack_a, t_node **head_stack_b);
-void		perform_movements(t_node **stack_a, t_node **stack_b);
-int			get_smaller_index_position(t_node *stack_a);
-void		error(void);
-void		check_numbers(t_node **stack_a);
 
 #endif
