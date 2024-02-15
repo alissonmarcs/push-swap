092781dd3f508eb84694a79f661b7652209ead0f
Alisson Marcos <alissonmarcos249@gmail.com>
2024-02-15 17:31:49 -0300

    wip: remove debug function

--- mandatory/main.c
+++ mandatory/main.c
@@ -6,13 +6,11 @@
 /*   By: almarcos <almarcos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 08:59:51 by almarcos          #+#    #+#             */
-/*   Updated: 2024/01/08 09:18:59 by almarcos         ###   ########.fr       */
+/*   Updated: 2024/02/15 17:31:30 by almarcos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-void	print_stack(t_node *head);
 
 int	main(int argc, char *argv[])
 {
@@ -27,15 +25,4 @@ int	main(int argc, char *argv[])
 	set_node_indexes(stack_a, get_list_size(stack_a));
 	sort(&stack_a, &stack_b);
 	free_list(&stack_a);
-}
-
-void	print_stack(t_node *head)
-{
-	while (head)
-	{
-		ft_printf("\t------------------------\n");
-		ft_printf("\t\tNumber: %d\n", head->number);
-		ft_printf("\t\tIndex: %d\n", head->index);
-		head = head->next;
-	}
 }
